cd70573f9feecf883630ebfbeea05e2754987cb6
jerome.wiedmann <jerome.wiedmann@103F25.etu.iut-lens.univ-artois.fr>
2012-12-10 11:20:10 +0100

    modification déplacement

---
+++ src/fr/dubois/space/invader/Alien.java
@@ -0,0 +1,17 @@
+package fr.dubois.space.invader;
+
+import android.graphics.Bitmap;
+
+class Alien extends Sprite {
+
+	public Alien(Bitmap bitmap, float x, float y) {
+		super(bitmap, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		x = x+2;
+	}
+}
